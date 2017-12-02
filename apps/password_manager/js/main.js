51fbcc5c9d3a01f5611dbf2e7a86ac8ba67bf4e5
evilsocket <evilsocket@gmail.com>
2017-12-02 16:36:27 +0100

    Threw some angularjs in there :)

--- apps/password_manager/js/main.js
+++ apps/password_manager/js/main.js
@@ -1,7 +1,68 @@
-var vault = new Vault( 'http', 'localhost', 8081 );
 var username = "vault";
 var password = "vault";
+var store    = "passwords";
 
+var app = angular.module('PM', [], function($interpolateProvider) {
+
+});
+
+app.controller('PMController', ['$scope', function (scope) {
+    scope.loginError = false;
+    scope.vault = new Vault();
+
+    scope.doLogin = function() {
+        var username = $('#username').val();
+        var password = $('#password').val();
+
+        scope.vault.Login( username, password, function(token) {
+            scope.loginError = false;
+            scope.$apply();
+        },
+        function(error){
+            scope.loginError = true;
+            scope.$apply();
+        });
+
+    }
+    /*
+    scope.update = function() {
+        $.get('/targets', function(data) {
+            if( data.stats.Progress < 100.0 || scope.firstTimeUpdate == false ) {
+                var start = new Date(data.stats.Start),
+                    stop = new Date(data.stats.Stop),
+                    dur = new Date(null);
+
+                dur.setSeconds( (stop-start) / 1000 );
+                scope.duration = dur.toISOString().substr(11, 8);
+            }
+            
+            scope.ntargets = Object.keys(scope.targets).length;
+
+            scope.applyFilters(data);
+
+            scope.targets = data.targets;
+            scope.domain = data.domain;
+            scope.stats = data.stats;
+            
+            document.title = "XRAY ( " + scope.domain + " | " + scope.stats.Progress.toFixed(2) + "% )";
+
+            scope.$apply();
+            scope.firstTimeUpdate = true;
+
+            $('.htoggle').each(function() {
+                $(this).click(function(e){
+                    $( $(this).attr('href') ).toggle();
+                    return false;
+                });
+            });
+        });
+    }
+
+    setInterval( scope.update, 500 );
+    */
+}]);
+
+/*
 $(function() {
     console.log( "Document ready, starting main.js ..." );
     
@@ -24,3 +85,4 @@ $(function() {
         console.log("Login error:\n" + data);
     });
 })
+*/
