8e52f425a1fa3632c3d4bb476fe7ff32aaa7791d
evilsocket <evilsocket@gmail.com>
2017-12-03 14:05:07 +0100

    balls

--- apps/password_manager/js/main.js
+++ apps/password_manager/js/main.js
@@ -67,6 +67,17 @@ app.filter('timeago', function() {
     }
 });
 
+// taken from https://gist.github.com/thomseddon/3511330
+app.filter('bytes', function() {
+    return function(bytes, precision) {
+        if (isNaN(parseFloat(bytes)) || !isFinite(bytes)) return '-';
+        if (typeof precision === 'undefined') precision = 1;
+        var units = ['B', 'kB', 'MB', 'GB', 'TB', 'PB'],
+            number = Math.floor(Math.log(bytes) / Math.log(1024));
+        return (bytes / Math.pow(1024, Math.floor(number))).toFixed(precision) +  ' ' + units[number];
+    }
+});
+
 app.controller('PMController', ['$scope', function (scope) {
     scope.statusMessage = null;
     scope.errorMessage = null;
