797a4e36d4f07b0b5834734d5d275c40ffb44a95
evilsocket <evilsocket@gmail.com>
2017-12-04 21:20:18 +0100

    balls

--- webapps/secrets_manager/js/entries/file.js
+++ webapps/secrets_manager/js/entries/file.js
@@ -20,7 +20,7 @@ FileEntry.prototype.formGroup = function(input, id) {
     return '<div class="form-group">' + 
              '<span class="label entry-title label-default label-' + this.type + '" id="editable_' + id + '" for="name_of_' + id + '">' + this.name + '</span>' +
              '<input class="blur editable-input hidden" type="text" id="name_of_' + id + '" value="' + this.name + '">' + 
-             '<label class="upload action-button btn btn-default" for="' + id + '"><i class="fa fa-upload" aria-hidden="true"></i>' +
+             '<label class="upload btn btn-default" for="' + id + '"><i class="fa fa-upload" aria-hidden="true"></i>' +
                 input +
              '</label>' +
             '</div>';
