20e3dcfbe8bd194da932d5251ea6edd27b419b9e
evilsocket <evilsocket@gmail.com>
2017-12-04 20:54:11 +0100

    Loading database from configured path.

--- models/init.go
+++ models/init.go
@@ -4,20 +4,33 @@ import (
 	"github.com/evilsocket/vault/config"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"log"
 	"os/user"
-	"path"
+	"path/filepath"
+	"strings"
 )
 
 var db *gorm.DB
 
-func Setup() error {
-	usr, err := user.Current()
-	if err != nil {
+func expand(path string) (string, error) {
+	if strings.HasPrefix(path, "~") {
+		usr, err := user.Current()
+		if err != nil {
+			return "", err
+		}
+		path = strings.Replace(path, "~", usr.HomeDir, -1)
+	}
+	return filepath.Abs(path)
+}
+
+func Setup() (err error) {
+	if config.Conf.Database, err = expand(config.Conf.Database); err != nil {
 		return err
 	}
 
-	filename := path.Join(usr.HomeDir, config.Conf.Database)
-	if db, err = gorm.Open("sqlite3", filename); err != nil {
+	log.Printf("Loading database %s ...\n", config.Conf.Database)
+
+	if db, err = gorm.Open("sqlite3", config.Conf.Database); err != nil {
 		return err
 	}
 
