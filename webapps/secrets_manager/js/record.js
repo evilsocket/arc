5b8cb8310e0fc7794324bda6d90757b8a653d148
evilsocket <evilsocket@gmail.com>
2017-12-04 06:16:50 +0100

    Various fixes.

---
+++ webapps/secrets_manager/js/record.js
@@ -0,0 +1,60 @@
+function Record(title) {
+    this.title = title
+    this.entries = [];
+    this.error = null;
+}
+
+Record.prototype.AddEntry = function(entry) {
+    this.entries.push(entry);
+}
+
+Record.prototype.SetError = function(error) {
+    console.log( "RECORD ERROR: " + error);
+    this.entries = [];
+    this.error = error;
+}
+
+Record.prototype.HasError = function() {
+    return ( this.error != null );
+}
+
+Record.prototype.Encrypt = function( key ) {
+    for( var i = 0; i < this.entries.length; i++ ) {
+        this.entries[i].is_new = false;
+    }
+
+    var data = JSON.stringify(this.entries); 
+    console.log( "Encrypting " + data.length + " bytes of password." );
+    data = CryptoJS.AES.encrypt( data, key ).toString(); 
+    console.log( "Encrypted data is " + data.length + " bytes." );
+    return data
+}
+
+Record.prototype.Decrypt = function( key, data ) {
+    console.log( "Decrypting " + data.length + " bytes of record." );
+    try {
+        data = CryptoJS.AES.decrypt( data, key ).toString(CryptoJS.enc.Utf8); 
+    }
+    catch(err) {
+        this.SetError( "Error while decrypting record data." );
+        return;
+    }
+
+    console.log( "Decrypted data is " + data.length + " bytes." );
+
+    // quick and dirty check
+    if( data.indexOf('"value"') == -1 ) {
+        this.SetError( "Error while decrypting record data." );
+    } else {
+        var objects = JSON.parse(data);
+        
+        this.entries = [];
+        console.log( "Record has " + objects.length + " entries." );
+
+        for( var i = 0; i < objects.length; i++ ) {
+            this.entries.push( EntryFromObject(objects[i]) );
+            this.entries.is_new = false;
+        }
+    }
+}
+
