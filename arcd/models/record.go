8d2c3b8b5cebd8fb6049c8520b974fbe1b5bea57
evilsocket <evilsocket@gmail.com>
2017-12-09 11:10:57 +0100

    Restored normal sorting by updated_at desc

--- arcd/models/record.go
+++ arcd/models/record.go
@@ -59,7 +59,7 @@ func (r *Record) BeforeDelete() (err error) {
 }
 
 func Records(store_id string) (records []Record, err error) {
-	err = db.Where("store_id = ?", store_id).Order("expired_at desc, updated_at desc").Find(&records).Error
+	err = db.Where("store_id = ?", store_id).Order("updated_at desc").Find(&records).Error
 	return
 }
 
