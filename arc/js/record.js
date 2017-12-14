13877076a337ea07f662f8def296fc905bf43332
Raffaele Florio <raffaeleflorio@protonmail.com>
2017-12-14 16:47:33 +0100

    CryptoJs -> WebCrypto

--- arc/js/record.js
+++ arc/js/record.js
@@ -27,61 +27,40 @@ Record.prototype.HasError = function() {
 
 /* Return a Promise */
 Record.prototype.Encrypt = function( key ) {
-    for( var i = 0; i < this.entries.length; i++ ) {
-        this.entries[i].is_new = false;
-    }
+    this.entries.forEach((e) => e.is_new = false);
 
-    var data = JSON.stringify(this.entries); 
+    const data = JSON.stringify(this.entries); 
+    console.log(`Encrypting ${data.length} bytes of record.`);
 
-    console.log( "Encrypting " + data.length + " bytes of record." );
-    // console.log(data);
-
-    /* currenlty fake Promise */
-    return Promise.resolve(AESEncrypt(data, key))
-	.then((encrypted) => {
-	    console.log( "Encrypted data is " + encrypted.length + " bytes." );
-	    return encrypted;
-	});
+    return AESEncrypt2(data, key).then((encrypted) => {
+	console.log(`Encrypted data is ${encrypted.length} bytes.`);
+	return encrypted;
+    });
 }
 
 Record.prototype.isValidData = function(data) {
     return ( data == "[]" || data.indexOf('"value"') != -1 );
 }
 
-/* Return a Promise */
 Record.prototype.Decrypt = function( key, data ) {
-    console.log( "Decrypting " + data.length + " bytes of record." );
-    // console.log(data);
-
+    console.log(`Decrypting ${data.length} bytes of record.`);
     const that = this;
-    /* currently fake Promise */
-    return Promise.resolve(AESDecrypt(data, key))
-	.then((decrypted) => {
-	    try {
-		decrypted = decrypted.toString(CryptoJS.enc.Utf8);
-	    } catch (error) {
-		throw "Error while decrypting record data.";
-	    }
 
-	    console.log( "Decrypted data is " + decrypted.length + " bytes." );
+    return AESDecrypt2(data, key).then((decrypted) => {
+	console.log(`Decrypted data is ${decrypted.length} bytes.`);
 
-	    // quick and dirty check
-	    if( that.isValidData(decrypted) === false ) {
-		throw "Error while decrypting record data.";
-	    } else {
-		const objects = JSON.parse(decrypted);
+	// quick and dirty check
+	if (that.isValidData(decrypted) === false)
+	    throw "Error while decrypting record data.";
 
-		console.log( "Record has " + objects.length + " entries." );
+	const objects = JSON.parse(decrypted);
 
-		that.entries = [];
-		for( var i = 0; i < objects.length; i++ ) {
-		    var entry = TypeFactory(objects[i]);
-		    // console.log( "record.entries[" + i + "] = " + entry.TypeName() );
-		    that.entries.push(entry);
-		}
-	    }
-	}).catch((error) => {
-	    console.error(error);
-	    that.setError(error);
-	});
+	console.log(`Record has ${objects.length} entries.`);
+
+	that.entries = [];
+	objects.forEach((o) => that.entries.push(TypeFactory(o)));
+    }).catch((error) => {
+	console.error(error);
+	that.setError(error);
+    });
 }
---
+++ arc/js/webcrypto.js
@@ -0,0 +1,80 @@
+const crypto_utils = {
+    /* size in bytes */
+    getRandomValues: (size) => {
+	const buffer = new Uint8Array(size);
+	window.crypto.getRandomValues(buffer);
+	return buffer;
+    },
+
+    getBaseKey: (
+	keyData,
+	algo,
+	format = "raw"
+    ) => window.crypto.subtle.importKey(
+	format,
+	keyData,
+	algo,
+	false,
+	["deriveKey"]
+    ),
+
+    getDerivedKey: (
+	keyData, baseKeyAlgo, format,
+	derivationAlgo,
+	derivatedKeyAlgo,
+	usage,
+	extractable = false
+    ) => crypto_utils.getBaseKey(keyData, baseKeyAlgo, format).then((baseKey) => {
+	return crypto.subtle.deriveKey(
+	    derivationAlgo,
+	    baseKey,
+	    derivatedKeyAlgo,
+	    extractable,
+	    usage
+	)
+    }),
+};
+
+Uint8Array.prototype.toHex = function() {
+    let hex = "";
+    this.forEach((x) => hex += ("00" + x.toString(16)).slice(-2));
+    return hex;
+};
+Uint8Array.prototype.toBase64 = function() {
+    let s = "";
+    this.forEach((x) => s += String.fromCharCode(x));
+    return btoa(s);
+};
+Uint8Array.fromHex = function(hex) {
+    const uint8 = new Uint8Array(hex.length / 2);
+    for (let i = 0, j = 0; i <= hex.length - 2; i += 2, j++)
+	uint8[j] = parseInt('' + hex[i] + hex[i + 1], 16);
+
+    return uint8;
+};
+Uint8Array.fromBase64 = function(base64) {
+    base64 = atob(base64);
+    const uint8 = new Uint8Array(base64.length);
+    for (let i = 0; i < base64.length; ++i)
+	uint8[i] = base64.charCodeAt(i);
+    return uint8;
+};
+
+/* get a key (and related salt) using pbkdf2 */
+const pbkdf2 = ({
+    passphrase,
+    salt = crypto_utils.getRandomValues(16),
+    iterations = 100,
+    hash = "SHA-256",
+    derivedKeyAlgo,
+    usage
+} = {}) => crypto_utils.getDerivedKey(
+    new TextEncoder("utf-8").encode(passphrase), {"name": "PBKDF2"}, "raw",
+    {"name": "PBKDF2", salt, iterations, hash},
+    derivedKeyAlgo,
+    usage,
+    false
+).then((key) => {
+    key.salt = salt;
+    return key;
+});
