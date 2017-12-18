8ea0c3deabca3b5b942a641038c51e7817dfea66
evilsocket <evilsocket@gmail.com>
2017-12-18 18:16:05 +0100

    refact: using go template engine to generate email reports.

--- arcd/events/const.go
+++ arcd/events/const.go
@@ -10,18 +10,33 @@ package events
 import (
 	"fmt"
 	"github.com/evilsocket/arc/arcd/db"
-	"html"
+	"time"
 )
 
 func Login(successful bool, address string, username string, password string) Event {
 	if successful {
-		desc := fmt.Sprintf("Address %s successfully logged into the Arc server.", address)
+		desc := Populate(TPL_LOGIN_OK, struct {
+			When    string
+			Address string
+		}{
+			time.Now().Format(TPL_DATE_FORMAT),
+			address,
+		})
+
 		return New("login_ok", "Successful login.", desc)
 	} else {
-		desc := fmt.Sprintf("Address <b>%s</b> tried to log into the Arc server with username <b>%s</b> and password <b>%s</b>.",
-			html.EscapeString(address),
-			html.EscapeString(username),
-			html.EscapeString(password))
+		desc := Populate(TPL_LOGIN_KO, struct {
+			When     string
+			Address  string
+			Username string
+			Password string
+		}{
+			time.Now().Format(TPL_DATE_FORMAT),
+			address,
+			username,
+			password,
+		})
+
 		return New("login_ko", "Failed login attempt.", desc)
 	}
 }
@@ -30,13 +45,20 @@ func InvalidToken(address, auth string, err error) Event {
 	title := "Invalid token authentication."
 	reason := ""
 	if err != nil {
-		reason = fmt.Sprintf(" (reason: %s)", err.Error())
+		reason = err.Error()
 	}
 
-	desc := fmt.Sprintf("Address <b>%s</b> tried to authenticate with an invalid token '%s'%s.",
-		html.EscapeString(address),
-		html.EscapeString(auth),
-		reason)
+	desc := Populate(TPL_TOKEN_KO, struct {
+		When    string
+		Address string
+		Reason  string
+		Token   string
+	}{
+		time.Now().Format(TPL_DATE_FORMAT),
+		address,
+		reason,
+		auth,
+	})
 
 	return New("token_ko", title, desc)
 }
@@ -45,34 +67,39 @@ func RecordExpired(r *db.Record) Event {
 	meta := r.Meta()
 
 	title := fmt.Sprintf("'%s' just expired.", meta.Title)
-	compressed := ""
-	deleted := ""
-
-	if meta.Compressed {
-		compressed = "(and gzipped) "
-	}
-
-	if meta.Prune {
-		deleted = "It has been deleted from the system."
-	}
-
-	desc := fmt.Sprintf("The record <b>%s</b> which was created on %s and updated on %s just expired, it was made of %d bytes of %s encrypted %sdata.%s",
-		html.EscapeString(meta.Title),
-		meta.CreatedAt.Format("Mon Jan 2 15:04:05 2006"),
-		meta.UpdatedAt.Format("Mon Jan 2 15:04:05 2006"),
+	desc := Populate(TPL_RECORD_EXPIRED, struct {
+		When       string
+		Title      string
+		CreatedAt  string
+		UpdatedAt  string
+		Size       uint64
+		Encryption string
+		Compressed bool
+		Deleted    bool
+	}{
+		time.Now().Format(TPL_DATE_FORMAT),
+		meta.Title,
+		meta.CreatedAt.Format(TPL_DATE_FORMAT),
+		meta.UpdatedAt.Format(TPL_DATE_FORMAT),
 		meta.Size,
 		meta.Encryption,
-		compressed,
-		deleted)
+		meta.Compressed,
+		meta.Prune,
+	})
 
 	return New("record_expired", title, desc)
 }
 
 func UpdateAvailable(curr_ver, new_ver, url string) Event {
 	title := fmt.Sprintf("Arc v%s is available.", new_ver)
-	desc := fmt.Sprintf("You are running Arc v%s but v%s is available <a href=\"%s\">for download</a>.",
+	desc := Populate(TPL_UPDATE, struct {
+		Version    string
+		NewVersion string
+		Link       string
+	}{
 		curr_ver,
 		new_ver,
-		url)
+		url,
+	})
 	return New("update", title, desc)
 }
