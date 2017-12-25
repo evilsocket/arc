30fa29846fa16ea6bce97922e7381c109161c1f0
jemacom <jemacomchokri@gmail.com>
2017-12-25 16:56:00 +0100

    Add comments for errors functions

--- arcd/utils/json.go
+++ arcd/utils/json.go
@@ -12,8 +12,9 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// Used when sending a HTTP status response different than 200
+// ErrorResponse is used when sending a HTTP status response different than 200
 // swagger:response errorResponse
+// Code reference  http://www.restapitutorial.com/httpstatuscodes.html
 type ErrorResponse struct {
 	// The error description.
 	// in: body
@@ -33,7 +34,7 @@ func jError(level int, c *gin.Context, code int, message string) {
 	c.Abort()
 }
 
-// http://www.restapitutorial.com/httpstatuscodes.html
+// NotFound log 404 error message in JSON format
 func NotFound(c *gin.Context, opt_msg ...string) {
 	msg := "Not found."
 	if len(opt_msg) > 0 {
@@ -42,6 +43,7 @@ func NotFound(c *gin.Context, opt_msg ...string) {
 	jError(log.WARNING, c, 404, msg)
 }
 
+// BadRequest log 400 eror message in JSON format
 func BadRequest(c *gin.Context, opt_msg ...string) {
 	msg := "Bad request."
 	if len(opt_msg) > 0 {
@@ -50,6 +52,7 @@ func BadRequest(c *gin.Context, opt_msg ...string) {
 	jError(log.WARNING, c, 400, msg)
 }
 
+// Forbidden log 403 error message in JSON format
 func Forbidden(c *gin.Context, opt_msg ...string) {
 	msg := "Forbidden"
 	if len(opt_msg) > 0 {
@@ -58,6 +61,7 @@ func Forbidden(c *gin.Context, opt_msg ...string) {
 	jError(log.WARNING, c, 403, msg)
 }
 
+// ServerError log 500 error message in JSON format
 func ServerError(c *gin.Context, err error) {
 	jError(log.ERROR, c, 500, "Error while performing operation: "+err.Error())
 }
